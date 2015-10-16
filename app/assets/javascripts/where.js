e3b4b85558609c560642e5efd1ee0a08d765e2d9
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-16 12:30:59 -0700

    update color schemes and add nav bar color

--- app/assets/javascripts/where.js
+++ app/assets/javascripts/where.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
+$(document).on('ready', function(){
 	console.log("inside the where")
 	showMap();
 })
