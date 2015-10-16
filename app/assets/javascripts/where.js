d1b2136f3f5d9be65a708dba0e60a68c499d42f7
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-15 21:31:00 -0700

    add map and information for lodging

--- app/assets/javascripts/where.js
+++ app/assets/javascripts/where.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+	showMap();
+})
+
+var showMap = function(){
+	var map;
+
+	var myLatLng = {
+		lat: 26.137409, 
+		lng: -80.109059
+	};
+	var map = new google.maps.Map(document.getElementById('googleMap'), {
+	    center: myLatLng,
+	    zoom: 14
+	  });
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+    map: map,
+    title: 'Wedding Spot'
+  	});
+}
