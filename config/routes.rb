116e76e1369d50a0a0541156e3f0c5f7cde96c73
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-20 09:24:18 -0700

    add routes for charges with stripe and views for new charges

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'story/index'
   get 'where/index'
 
-  
   resources :playlists
+  resources :charges
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
