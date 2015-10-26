60e7c355b8580f479c699566b0b9afca28d413bb
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-26 14:59:41 -0700

    setup stripe controller and routes to handle registry payment

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,29 @@
+class ChargesController < ApplicationController
+
+def new
+end
+
+def create
+  # Amount in cents
+  @amount = 100
+
+  customer = Stripe::Customer.create(
+    :email => params[:email],
+    :card  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Wedding Guest customer',
+    :currency    => 'usd'
+  )
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to registry_path
+end
+
+end
+
+
