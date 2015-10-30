2fbfd6a1d39dc349901e82d06adb1344abd47038
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-30 11:40:00 -0700

    add errors for form and set nested attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
 	has_many :playlists
 
+	accepts_nested_attributes_for :playlists
+
 	validates :email, presence: true, uniqueness: true
 	validates :name, presence: true, uniqueness: true
 	validates :rsvp_response, presence: true
