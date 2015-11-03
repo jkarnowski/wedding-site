e127dfba8a25ba826811fa931fdfea9706a436c7
jkarnowski <jaclyn.karnowski@gmail.com>
2015-11-02 16:55:43 -0800

    update schema for remaining cost to fund

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030231448) do
+ActiveRecord::Schema.define(version: 20151103003149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(version: 20151030231448) do
     t.string   "title"
     t.string   "description"
     t.integer  "total_cost"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "image"
     t.integer  "desired_amount"
     t.integer  "multiplier"
+    t.integer  "remaining_amount"
   end
 
   create_table "users", force: :cascade do |t|
