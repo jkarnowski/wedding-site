b00c7b2e9a1aa34cbe0e12d109e5f02fe32a9486
jkarnowski <jaclyn.karnowski@gmail.com>
2015-12-14 11:18:22 -0800

    display stripe form on button submit

--- app/assets/javascripts/charges.js
+++ app/assets/javascripts/charges.js
@@ -2,37 +2,75 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
 
-	var handler = StripeCheckout.configure({
-		key: 'pk_test_UlUbDYdUkO0qkJ2r1Iw3DYtZ',
-		token: function(token) {
-			$('#stripeToken').val(token.id);
+	console.log("READY!");
+
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_UlUbDYdUkO0qkJ2r1Iw3DYtZ',
+    locale: 'auto',
+    token: function(token) {
+      // Use the token to create the charge with a server-side script.
+      // You can access the token ID with `token.id`
+      $('#stripeToken').val(token.id);
 			$('#stripeEmail').val(token.email);
 			$('#stripe-form').submit();
-		}
-	});
+    }
+  });
 
-	$('.stripeButton').on('click', function(event){
-		var centsFromStripeForm= "#amount" + event.toElement.dataset.index;
+  $('.stripeButton').on('click', function(event) {
 
-		console.log(centsFromStripeForm);
+  	var centsFromStripeForm= ".amount_" + event.toElement.dataset.index;
+  	console.log(centsFromStripeForm);
 
-		var amount = $(centsFromStripeForm).val() * 100;
-		console.log(amount);
-		var displayAmount = parseFloat(Math.floor($(idOfTheFormElements).val() * 100) / 100).toFixed(2);
-	});
-	console.log(displayAmount);
-
-		// Open Checkout with further options
+  	var amount = $(centsFromStripeForm).val() * 100;
+  	console.log(amount);
+    // Open Checkout with further options
     handler.open({
-      name: 'Honeymoon Fund',
-      description: 'help fund happiness',
-      amount: amount,
+      name: 'Jaclyn Karnowski & Pablo Perez',
+      description: 'Honeymoon Fund',
+      amount: amount
     });
     event.preventDefault();
-	});
+  });
 
-	// // Close Checkout on page navigation
-	$(window).on('popstate', function () {
-	  handler.close();
-	});
-})
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
+  });
+
+	// var handler = StripeCheckout.configure({
+	// 	key: 'pk_test_UlUbDYdUkO0qkJ2r1Iw3DYtZ',
+	// 	token: function(token) {
+	// 		$('#stripeToken').val(token.id);
+	// 		$('#stripeEmail').val(token.email);
+	// 		$('#stripe-form').submit();
+	// 	}
+	// });
+
+	// $('.stripeButton').on('click', function(event){
+	// 	console.log("inside EVENT ON CLICK!")
+
+	// 	var centsFromStripeForm= "#amount" + event.toElement.dataset.index;
+
+	// 	console.log(centsFromStripeForm);
+
+	// 	var amount = $(centsFromStripeForm).val() * 100;
+	// 	console.log(amount);
+	// 	// var displayAmount = parseFloat(Math.floor($(idOfTheFormElements).val() * 100) / 100).toFixed(2);
+	// });
+
+	// 	// Open Checkout with further options
+ //    handler.open({
+ //      name: 'Jaclyn Karnowski & Pablo Perez Honeymoon Fund',
+ //      description: 'help fund happiness',
+ //      amount: amount,
+ //    });
+ //    event.preventDefault();
+	// });
+
+	// // // Close Checkout on page navigation
+	// $(window).on('popstate', function () {
+	//   handler.close();
+	// });
+});
+
+
