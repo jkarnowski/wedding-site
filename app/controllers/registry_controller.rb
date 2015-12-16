4e1efe5356d9f154f7a213a6824276d3ebc4b3e8
jkarnowski <jaclyn.karnowski@gmail.com>
2015-12-16 14:16:30 -0800

    remove unnecessary items array

--- app/controllers/registry_controller.rb
+++ app/controllers/registry_controller.rb
@@ -2,18 +2,6 @@ class RegistryController < ApplicationController
   
   def index
   	@items = Registry.all
-
-  	total_cost = []
-
-  	@items.each do |item|
-  		total_cost << item.total_cost
-  	end
-
-  	if request.xhr?
-  		render json: total_cost, status: 200, layout: false
-  	else
-  		render 'index'
-  	end
   end
   
 end
